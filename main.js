00acd35b4ad341aaba724bab566e4954d3e84060
Alert <faceroxz@gmail.com>
2021-01-23 16:44:02 +0700

    Test

---
+++ main.js
@@ -0,0 +1,83 @@
+let playerScore = 0;
+let pcScore = 0;
+let playerScore_div = document.getElementById("player-score");
+let pcScore_div = document.getElementById("pc-score");
+let result_div = document.getElementById("result");
+let rock_div = document.getElementById("r");
+let paper_div = document.getElementById("p");
+let scissors_div = document.getElementById("s");
+
+
+function getPCChoice(){
+    const choices = ['r','p','s'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function convert(letter){
+if(letter === 'r') return "Rock";
+if(letter === 'p') return "Paper";
+return "Scissors";
+}
+
+function win(userChoice, pcChoice){
+    playerScore++;
+    playerScore_div.innerHTML = playerScore;
+    pcScore_div.innerHTML = pcScore;
+    result_div.innerHTML = "WIN"
+    document.getElementById(userChoice).classList.add('winner');
+}
+
+function lose(userChoice, pcChoice){
+    pcScore++;
+    playerScore_div.innerHTML = playerScore;
+    pcScore_div.innerHTML = pcScore;
+    result_div.innerHTML = "LOST";
+    document.getElementById(userChoice).classList.add('loser');
+}
+
+function draw(userChoice, pcChoice){
+    result_div.innerHTML = "EQUAL";
+    document.getElementById(userChoice).classList.add('draw');
+}
+
+function game(userChoice){
+    const pcChoice = getPCChoice();
+
+    switch(userChoice + pcChoice){
+        case "rs":
+        case "pr":
+        case "sp":
+        win(userChoice, userChoice);
+        break;
+        case "rp":
+        case "ps":
+        case "sr":
+        lose(userChoice, userChoice);
+        break;
+        case "rr":
+        case "pp":
+        case "ss":
+        draw(userChoice, userChoice);
+        break;
+    }
+}
+
+function main(){
+rock_div.addEventListener('click', function(){
+    rock_div.style.backgroundColor == "rgb(244,113,33)"
+    game('r');
+})
+
+paper_div.addEventListener('click', function(){
+    rock_div.style.backgroundColor == "rgb(244,113,33)"
+    game('p');
+})
+
+scissors_div.addEventListener('click', function(){
+    rock_div.style.backgroundColor == "rgb(244,113,33)"
+    game('s');
+})
+}
+
+main();
